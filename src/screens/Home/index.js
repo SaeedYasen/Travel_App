09f7c20d7bc4fbfb940440343fd6cfe2e20ff709
saeed <saeedyasen41@gmail.com>
2024-02-29 16:54:45 +0200

    Set Up Project

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,18 +1,12 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { View, SafeAreaView} from "react-native";
 import Title from "../../components/Title";
 import TitleClass from "../../components/Title/classComponent";
 const Home=()=>{
-    const[title,setTitle]=useState('My First Component')
-    useEffect(()=>{
-        setTimeout(()=>{
-            setTitle('Updated Prop')
-        },3000);
-    },[]);
     return (
         <SafeAreaView>
             <View>
-                <Title text={title}/>
+                <Title />
                 <TitleClass text='class component' />
             </View>
         </SafeAreaView>
